b8e828ec3bf09c560e5870fb8c33d598f3da1bad
Bakkesh KS <bakkeshpruthvi49@gmail.com>
2023-09-26 01:34:53 +0530

    Completed: count elements assignment

---
+++ Week-8/Assignment8.6/countElements.js
@@ -0,0 +1,9 @@
+/*
+Problem: Take an array input from the user and return the count of elements.
+Note - The time complexity must be O(1). So you cannot use a loop.
+ */
+function countElements(arr) {
+  return arr.length;
+}
+
+console.log(countElements([1, 2, 3, 4, 5]));
