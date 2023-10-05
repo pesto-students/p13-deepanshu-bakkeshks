fbef1af95c37020c421617777bb239927250df71
Bakkesh KS <bakkeshpruthvi49@gmail.com>
2023-10-05 10:12:52 +0530

    Completed: string reverse recursion

---
+++ Week-9/Assignment9.14/stringReverseRecursion.js
@@ -0,0 +1,20 @@
+/*
+Problem: String Reversal using Recursion
+Input: N="hello", Output: olleh
+Steps: Reverse the substring from the 2nd character to the end, then concatenate the first character at the end.
+    1] Base Case: If the input string is empty or has a length of 1, it is already reversed. Return the string itself.
+    2] Recursive Case: For strings longer than 1 character, take the first character out, reverse the rest of the 
+    string using recursion, and concatenate the first character at the end.
+*/
+
+function reverseString(str) {
+  if (str.length <= 1) {
+    return str;
+  }
+
+  return reverseString(str.slice(1)) + str[0];
+}
+
+let inputString = "hello";
+let reversedString = reverseString(inputString);
+console.log(reversedString);
