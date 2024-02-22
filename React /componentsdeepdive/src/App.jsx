b39caf2b2d803cb73e881db8838aa6c4bc09a610
bakkeshks <bakkeshpruthvi49@gmail.com>
2024-02-22 17:21:51 +0530

    Completed: componentsdeepdive ass

---
+++ React /componentsdeepdive/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import BookList from './components/BookList' 
+import BookDetail from "./components/BookDetail"
+ 
+function App() {
+   
+  return (
+    <>
+       <BookList/>
+       <BookDetail/>
+   
+    </>
+  )
+}
+
+export default App
