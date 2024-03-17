b2ca894ef545aa790360ede242f0943dc89ef39b
bakkeshks <bakkeshpruthvi49@gmail.com>
2024-03-17 20:03:43 +0530

    Changed project name

--- React /hooks/src/hooks/useBookFilter.jsx
+++
@@ -1,8 +0,0 @@
-import { useMemo } from 'react';
-
-export const useBookFilter = (books, searchTerm) => {
-  return useMemo(() => {
-    if (!searchTerm) return books;
-    return books.filter(book => book.title.toLowerCase().includes(searchTerm.toLowerCase()));
-  }, [books, searchTerm]);
-};
--- React /hooks/src/hooks/useBookSorter.jsx
+++
@@ -1,7 +0,0 @@
-import { useMemo } from 'react';
-
-export const useBookSorter = (books) => {
-  return useMemo(() => {
-    return books.slice().sort((a, b) => a.title.localeCompare(b.title));
-  }, [books]);
-};
